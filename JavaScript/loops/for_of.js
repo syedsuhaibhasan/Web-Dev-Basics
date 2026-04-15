0ef72919b83ee1871a1bc28bee9c40628cb08aff
syedsuhaibhasan <syedsuhaibhasan888@gmail.com>
2026-04-16 04:21:02 +0500

    commit

---
+++ JavaScript/loops/for_of.js
@@ -0,0 +1,10 @@
+const arr = [1,2,3,4,5];
+// for each loop
+for (const itr of arr) {
+    console.log(itr)
+}
+
+const str = "hello world!";
+for (const newProg of str) {
+    console.log(newProg);
+}
