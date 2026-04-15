ac48a4f60392f15e109d31aaad015639c6a66ab2
syedsuhaibhasan <syedsuhaibhasan888@gmail.com>
2026-04-16 04:52:45 +0500

    Refactor for_each.js: Comment out unused friends array and related functions; enhance personBasedSkill output

--- JavaScript/loops/for_each.js
+++ JavaScript/loops/for_each.js
@@ -1,4 +1,37 @@
 const friends = ["ibrahim","ahmed","khurram","aliza"]
-friends.forEach((friend) => {
-    console.log(friend);
+// friends.forEach((friend) => {
+//     console.log(friend);
+// });
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// friends.forEach(printMe);
+
+const personBasedSkill = [
+    {
+        "name":"Ahmed",
+        "age":22,
+        "skill":"Agentic Ai"
+    },
+    {
+        "name":"Khurram",
+        "age":21,
+        "skill":"Data Science"
+    },
+    {
+        "name":"Wasiq",
+        "age":22,
+        "skill":"MERN"
+    },
+    {
+        "name":"Visam",
+        "age":24,
+        "skill":"React Native"
+    },
+]
+
+personBasedSkill.forEach((item,index)=>{
+    console.log(`Mr. ${item.name} has ${item.skill} skill`);
 });
