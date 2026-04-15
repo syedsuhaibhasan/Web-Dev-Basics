193a4f53c821c23a4016210e67a0ce75206c2246
syedsuhaibhasan <syedsuhaibhasan888@gmail.com>
2026-04-16 04:29:36 +0500

    commit

--- JavaScript/loops/for_of.js
+++ JavaScript/loops/for_of.js
@@ -1,10 +1,34 @@
-const arr = [1,2,3,4,5];
-// for each loop
-for (const itr of arr) {
-    console.log(itr)
+// const arr = [1,2,3,4,5];
+// // for each loop
+// for (const itr of arr) {
+//     console.log(itr)
+// }
+
+// const str = "hello world!";
+// for (const newProg of str) {
+//     console.log(newProg);
+// }
+
+// Maps
+// const map = new Map();
+// map.set('IN',"INDIA");
+// map.set('PK',"PAKISTAN");
+// map.set('IR',"IRAN");
+
+// console.log(map);
+
+// for (const [mapper,mapped] of map) {
+//     console.log(mapper,mapped);
+// }
+
+// On objects
+const obj = {
+    'name1':"Syed ",
+    'name2':"Muhammad ",
+    'name3':"Suhaib ",
+    'name4':"Hasan"
 }
 
-const str = "hello world!";
-for (const newProg of str) {
-    console.log(newProg);
+for (const [key,value] of obj) {
+    console.log(key,value);
 }
