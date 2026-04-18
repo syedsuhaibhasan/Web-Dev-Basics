5211d162f99ea3aa5ed995cd17a4b08de3a73011
syedsuhaibhasan <syedsuhaibhasan888@gmail.com>
2026-04-18 10:15:49 +0500

    commit

--- JavaScript/loops/for_each.js
+++ JavaScript/loops/for_each.js
@@ -147,3 +147,10 @@ const myNumbers = [1,2,3,4,5,6,7,8,9];
 
 // console.log(newNums);
 
+// reduce
+const myNums = [1,2,3]
+const mytotal = myNums.reduce((acc,currVal) => {
+    return acc+currVal
+},0)
+
+console.log(mytotal);
