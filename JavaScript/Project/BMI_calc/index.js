412951fef941f9fc882eccd219f904038a2acc19
syedsuhaibhasan <syedsuhaibhasan888@gmail.com>
2026-05-01 23:29:40 +0500

    commit

---
+++ JavaScript/Project/BMI_calc/index.js
@@ -0,0 +1,30 @@
+const weight = document.querySelector('#height');
+const height = document.querySelector('#weight');
+const calculateBtn = document.querySelector('#calc') ;
+let weightVal = 0;
+let heightVal = 0;
+
+
+calculateBtn.addEventListener('click',calcBMI);
+
+function calcBMI(){
+    if (weightVal>0 && heightVal>0) {
+         const heightInMeters = heightVal / 100;
+        const bmi = (weightVal / (heightInMeters * heightInMeters)).toFixed(2);
+
+        const ans = document.querySelector('#answer');
+        ans.innerHTML='';
+
+        const bmiSpan = document.createElement('span');
+        bmiSpan.innerText = bmi; 
+        ans.appendChild(bmiSpan);
+    }
+}
+
+weight.addEventListener('input',(event)=>{
+    weightVal = parseFloat(event.target.value) || 0;
+});
+
+height.addEventListener('input',(event)=>{
+    heightVal = parseFloat(event.target.value) || 0;
+});
