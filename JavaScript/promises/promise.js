52882612614918c3d3954dfba5644f5feda08cb2
syedsuhaibhasan <syedsuhaibhasan888@gmail.com>
2026-05-07 23:22:11 +0500

    commit

--- JavaScript/promises/promise.js
+++ JavaScript/promises/promise.js
@@ -31,3 +31,78 @@ promiseThree.then(function(user){
     console.log(user);
     
 })
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        let error = true;
+        if (!error) {
+            resolve({username:"sohaib hasan", pwd:"123456"});
+        }else{
+            reject('ERROR Something went wrong');
+        }
+    }, 1000);
+});
+
+promiseFour
+.then( (user)=>{
+    console.log(user);
+    return user.username; 
+})
+.then((username) => {
+    console.log(username);
+})
+.catch(function(err){
+    console.log(err);
+}).finally(function(){
+    console.log("The promise is either resolved or rejected");
+    
+});
+
+const promiseFive = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = true;
+        if (!error) {
+            resolve({username:"Java", pwd:"1234"});
+        }else{
+            reject('Java Something went wrong');
+        }
+    }, 1000);
+});  
+
+//another wya to resolve promises
+async function consumePromiseFive(){
+  try {
+     const response =  await promiseFive;
+   console.log(response);
+  
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
+
+// using fetch to call api
+async function getAllusers() {
+    try {
+        const response = await fetch('https://api.github.com/users/syedsuhaibhasan');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getAllusers();
+
+// using then() to get response
+fetch('https://api.github.com/users/syedsuhaibhasan')
+.then((response) => {
+    return response.json();
+})
+.then((response) => {
+    console.log(response);
+    
+})
+.catch((err) => {console.log(err);
+})
