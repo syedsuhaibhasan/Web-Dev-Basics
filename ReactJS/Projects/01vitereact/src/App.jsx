1979ee986bc305b34b66f0a5e21ca4e97c00540c
syedsuhaibhasan <syedsuhaibhasan888@gmail.com>
2026-06-24 14:20:42 -0700

    started react with vite and simple react

---
+++ ReactJS/Projects/01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>react with vite start | Syed Muhammad Suhaib Hasan</h1>
+  )
+}
+
+export default App
---
+++ ReactJS/basics-react-01/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>hellow world</h1>
+  );
+}
+
+export default App;
